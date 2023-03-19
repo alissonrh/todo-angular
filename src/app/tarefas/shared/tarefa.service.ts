1c4ad2383afdd3a6f4ad6428960e507931cdb6cd
alissonRH <alisson.honorato@live.com>
2023-03-19 19:40:49 -0300

    service pronta

--- src/app/tarefas/shared/tarefa.service.ts
+++ src/app/tarefas/shared/tarefa.service.ts
@@ -13,5 +13,43 @@ export class TarefaService {
     return tarefas ? JSON.parse(tarefas) : [];
   }
 
+  cadastrarTarefa(tarefa: Tarefa): void {
+    let tarefas = this.listarTodos();
+    tarefa.id = new Date().getTime();
+    tarefas = [...tarefas, tarefa];
+    localStorage['tarefas'] = JSON.stringify(tarefas)
+  }
+
+  buscarTarefaPorId(id: number): Tarefa {
+    const tarefas: Tarefa[] = this.listarTodos();
+    return tarefas.find(tarefa => tarefa.id === id);
+  }
+
+  atualizarTarefa(tarefa: Tarefa): void {
+    const tarefas: Tarefa[] = this.listarTodos();
+    tarefas.forEach((obj, index, objs) => { // ta verificando a tarefa, o index, e a lista das tarefas
+      if (tarefa.id === obj.id) {
+        objs[index] = tarefa;
+      }
+    });
+    localStorage['tarefas'] = JSON.stringify(tarefas)
+  }
+
+  removerTarefa(id: number): void {
+    let tarefas: Tarefa[] = this.listarTodos();
+    tarefas = tarefas.filter(tarefa => tarefa.id !== id)
+    localStorage['tarefas'] = JSON.stringify(tarefas)
+  }
+
+  alterarStatusDaTarefa(id: number): void {
+    const tarefas: Tarefa[] = this.listarTodos();
+    tarefas.map(tarefa => {
+      if (tarefa.id === id) {
+        tarefa.concluida = !tarefa.concluida;
+      }
+    });
+    localStorage['tarefas'] = JSON.stringify(tarefas)
+  }
+
   
 }
